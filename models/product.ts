cc1a8ef634b2cb21ec00847fff4c9182e003dc60
Paula <PaulaEmad00@gmail.com>
2024-10-14 21:52:17 +0300

    Init

---
+++ models/product.ts
@@ -0,0 +1,33 @@
+
+/*
+  [Product Management]
+    - Products have categories, names, descriptions, prices, and images (optional to use URLs for simplicity).
+*/
+
+// const productSchema = new mongoose.Schema({
+//     category: {
+//       type: String,
+//       required: [true, 'product category must be provided'],
+//     },
+
+//     name: {
+//       type: String,
+//       required: [true, 'product name must be provided'],
+//     },
+
+//     description: {
+//       type: String,
+//       required: [true, 'product description must be provided'],
+//     },
+
+//     price: {
+//       type: Number,
+//       required: [true, 'product price must be provided'],
+//     },
+
+//     Image: {
+//       type: String,
+//     }
+//   })
+  
+  
